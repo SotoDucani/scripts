335e8d2c6c57773acee90d845307631f0273ec80
Joshua Herrmann <jdvh1292@gmail.com>
2016-08-04 13:57:54 -0400

    Added basic comments

--- New-CredXML.ps1
+++ New-CredXML.ps1
@@ -1,2 +1,15 @@
-﻿$Cred = Get-Credential
+<#
+.SYNOPSIS
+  Creates a new clixml file of a credential provided and saves it to a specified path
+.NOTES
+  File NameL New-CredXML.ps1
+  Author: Joshua Herrmann
+  Written for: Powershell 3.0
+  Version: 1.0
+#>
+
+# Get the Credential Object
+$Cred = Get-Credential
+
+# Export the Credential as a clixml to the specified path
 Export-Clixml -InputObject $Cred -Path '\\XXXXXXXXXX\IT Admin\Credential XMLs\NEWCRED.clixml'
