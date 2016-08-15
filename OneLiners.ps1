e75d73844dc74ac842a3db39f0636d5649bbb119
Joshua Herrmann <jdvh1292@gmail.com>
2016-08-15 10:58:27 -0400

    Update OneLiners.ps1
    Added pingsweep

--- OneLiners.ps1
+++ OneLiners.ps1
@@ -2,3 +2,6 @@
 
 # Get-ADUsers while excluding certain OUs
 Get-ADUser -Filter * | ?{($_.DistinguishedName -notlike '*OU=Users,DC=company,DC=corp') -or ('*OU=Other Users,DC=company,DC=corp')}
+
+# Do a pingsweep of a specified address range
+1..255 | % {echo "192.168.0.$_" ; ping -n 1 -w 100 192.168.0.$_ | Select-String ttl}
