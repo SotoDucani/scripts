e52558c710b4010086db741797616ead735ac165
Joshua Herrmann <jdvh1292@gmail.com>
2016-08-23 10:52:27 -0400

    Update Grant-O365MailboxAccess.ps1

--- O365Scripts/Grant-O365MailboxAccess.ps1
+++ O365Scripts/Grant-O365MailboxAccess.ps1
@@ -1,11 +1,6 @@
 <#
 .SYNOPSIS
 	Grant Admin full access to all user mailboxes. You do not need to start a O365 session before running this script.
-.NOTES
-	File Name : Grant-O365MailboxAccess.ps1
-	Author : Joshua Herrmann
-	Written for : Powershell V3.0
-	Version : 1.0
 #>
 
 $UserCredential = Get-Credential
