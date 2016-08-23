29652bded6ef679cdef9190e49e7fc4880aef079
Joshua Herrmann <jdvh1292@gmail.com>
2016-08-23 10:47:38 -0400

    Update Backup-DomainControllers.ps1

--- ADScripts/Backup-DomainControllers.ps1
+++ ADScripts/Backup-DomainControllers.ps1
@@ -5,14 +5,14 @@
    Uses the built-in Windows Server Backup module to create the backup then copies that backup to 2 network locations.
 #>
 
-$FileSysCred = Import-Clixml '\\XXXXXXXXXX\IT Admin\Credential XMLs\BackupexecCred.clixml'
+$FileSysCred = Import-Clixml 'PATHTOCREDENTIALXML'
 
 # Create the backup
 wbadmin start systemstatebackup -backupTarget:'C:\Backups' -quiet
 
 # Create the needed PSDrives
-New-PSDrive -Name Z -PSProvider FileSystem -Root '\\YYYYYYYYYY\backup' -Credential $FileSysCred
-New-PSDrive -Name Y -PSProvider FileSystem -Root '\\XXXXXXXXXX\Y$' -Credential $FileSysCred
+New-PSDrive -Name Z -PSProvider FileSystem -Root 'BACKUPLOCATION1' -Credential $FileSysCred
+New-PSDrive -Name Y -PSProvider FileSystem -Root 'BACKUPLOCATION2' -Credential $FileSysCred
 
 # Copy the backup to target locations
 Copy-Item -Path C:\Backups -Destination Z:\ -Credential $FileSysCred -Recurse
