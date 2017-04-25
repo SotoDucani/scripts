753425a9893894e94e59c3d567bcd42f1a37af9e
Joshua Herrmann <jdvh1292@gmail.com>
2017-04-24 23:05:24 -0400

    Initial AppLocker Management Module Commit

---
+++ ADScripts/ApplockerManagement.psm1
@@ -0,0 +1,26 @@
+﻿<#
+.Synopsis
+Module made to allow cleaner and faster interaction with AppLocker policies. Also allows for mass modification to the policy while in the XML format.
+#>
+
+[xml]$policy = Get-AppLockerPolicy -Effective -Xml
+
+function Set-Policy {
+
+#parameters -Effective -LDAP -XML
+
+}
+
+function Show-Policy {
+
+#Parameters -Conditions -Names
+
+}
+
+function Remove-Versions {
+
+#ForEach-Object through and replace any entry in 
+
+$policy.AppLockerPolicy.RuleCollection[1].FilePublisherRule[0].Conditions.FilePublisherCondition.BinaryVersionRange.LowSection
+
+}
